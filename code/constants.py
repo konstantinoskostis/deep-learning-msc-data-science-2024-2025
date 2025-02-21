acdd2b57bd7225c14280bcf09f25d68560964e37
Konstantinos Kostis <kekostis@gmail.com>
2025-02-21 12:47:09 +0200

    Add a downloader utility for fashion-mnist dataset

--- code/constants.py
+++ code/constants.py
@@ -3,6 +3,10 @@
 TUNERS_DIR = 'tuners'
 MODELS_DIR = 'models'
 
+FASHION_MNIST_TRAIN_IMAGES_URL = 'http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-images-idx3-ubyte.gz'
+FASHION_MNIST_TRAIN_LABELS_URL = 'http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-labels-idx1-ubyte.gz'
+FASHION_MNIST_TEST_IMAGES_URL = 'http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-images-idx3-ubyte.gz'
+FASHION_MNIST_TEST_LABELS_URL = 'http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-labels-idx1-ubyte.gz'
 FASHION_MNIST_DATA_PATH = 'data/fashion_mnist'
 FASHION_MNIST_CLASS_IDS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 FASHION_MNIST_CLASS_LABELS = [
---
+++ code/fashion_mnist_downloader.py
@@ -0,0 +1,37 @@
+import subprocess
+import os
+from constants import (FASHION_MNIST_TRAIN_IMAGES_URL, FASHION_MNIST_TRAIN_LABELS_URL,
+                       FASHION_MNIST_TEST_IMAGES_URL, FASHION_MNIST_TEST_LABELS_URL)
+
+
+class FashioMNISTDownloader:
+    def __init__(self, download_directory):
+        self.download_directory = download_directory
+
+    def download(self):
+        if not os.path.isdir(self.download_directory):
+            os.mkdir(self.download_directory)
+
+        self.download_file(self.download_directory,
+                           FASHION_MNIST_TRAIN_IMAGES_URL)
+        self.download_file(self.download_directory,
+                           FASHION_MNIST_TRAIN_LABELS_URL)
+        self.download_file(self.download_directory,
+                           FASHION_MNIST_TEST_IMAGES_URL)
+        self.download_file(self.download_directory,
+                           FASHION_MNIST_TEST_LABELS_URL)
+
+        return
+
+    def download_file(self, download_directory, url):
+        try:
+            # check=True raises an exception on failure
+            subprocess.run(["wget", "-P", download_directory, url], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error downloading file: {e}")
+        except FileNotFoundError:
+            print("Error: wget is not installed. Please install it (e.g., 'sudo apt-get install wget' on Debian/Ubuntu).")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+
+        return
