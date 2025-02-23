d70ea6657ddb02a310957c1fb4ba7675f3d853b4
Konstantinos Kostis <kekostis@gmail.com>
2025-02-23 13:23:59 +0200

    [code/__init__.py] Update __init__.py by importing more classes

--- code/__init__.py
+++ code/__init__.py
@@ -1,6 +1,8 @@
 """A module that imports all needed classes"""
 
 from .constants import *
-from .fashion_mnist_loader import FashionMnistLoader
+from .fashion_mnist_loader import FashionMNISTLoader
+from .fashion_mnist_downloader import FashionMNISTDownloader
 from .mlp import (MLPTuner, MLP)
+from .cnn import (CNNTuner, CNN)
 from .evaluation_utils import (Metrics, EvaluationReport)
