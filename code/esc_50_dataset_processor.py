1652eaa87522eefa73a132f538069b1325c8a5cc
Konstantinos Kostis <kekostis@gmail.com>
2025-03-01 17:29:03 +0200

    [code/esc_50_dataset_processor.py] Allow normalization of mfcc and spectrograms

--- code/esc_50_dataset_processor.py
+++ code/esc_50_dataset_processor.py
@@ -1,5 +1,6 @@
 import librosa
 import numpy as np
+from tqdm.auto import tqdm
 
 from constants import SAMPLE_RATE, DURATION, N_MELS, N_MFCC, HOP_LENGTH
 
@@ -16,16 +17,22 @@ class ESC50DatasetProcessor:
         self.n_mfcc = n_mfcc
         self.hop_length = hop_length
 
-    def process(self, dataset):
+    def process(self, dataset,
+                normalize_spectrograms=True, normalize_mfcc=True):
         """Process a ESC-50 dataset slice by extracting audio features"""
 
-        for i, row in dataset.df.iterrows():
-            audio_features = self.extract_features(row['audio_path'])
+        for i, row in tqdm(dataset.df.iterrows(), total=len(dataset.df)):
+            audio_features = self.extract_features(
+                row['audio_path'],
+                normalize_spectrograms=normalize_spectrograms,
+                normalize_mfcc=normalize_mfcc
+            )
             dataset.append_audio_features(*audio_features)
 
         return dataset
 
-    def extract_features(self, audio_file_path):
+    def extract_features(self, audio_file_path,
+                         normalize_spectrograms=True, normalize_mfcc=True):
         """Extract audio features (Mel's spectogram and MFCCs)"""
 
         # Load audio
@@ -37,9 +44,15 @@ class ESC50DatasetProcessor:
             y=audio, sr=sr, n_mels=self.n_mels, hop_length=self.hop_length)
         mel_spec = librosa.power_to_db(mel_spec, ref=np.max)  # Convert to dB
 
+        if normalize_spectrograms is True:
+            mel_spec = (mel_spec - np.mean(mel_spec)) / np.std(mel_spec)
+
         # Compute MFCCs
         mfcc = librosa.feature.mfcc(y=audio, sr=sr,
                                     n_mfcc=self.n_mfcc,
                                     hop_length=self.hop_length)
+
+        if normalize_mfcc is True:
+            mfcc = (mfcc - np.mean(mfcc)) / np.std(mfcc)
 
         return mel_spec, mfcc
